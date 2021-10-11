42b354466a0e018d58c6b0edd995eef30d5b5125
Julian Lizarazo <julianca20122001@hotmail.com>
2021-10-11 17:01:57 -0500

    mclaren component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,12 @@ const routes: Routes = [
     path: "home",
     loadChildren: () => import("./home/home.module").then(m => m.HomeModule),
     
-  }
+  },
+  {
+    path: "mclaren",
+    loadChildren: () => import("./mclaren/mclaren.module").then(m => m.MclarenModule)
+    
+  },
 ];
 
 @NgModule({
