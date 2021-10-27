b476a45d24fb5977fc208b8cc44973bcc4c5c12f
Julian Lizarazo <julianca20122001@hotmail.com>
2021-10-27 17:00:12 -0500

    mclaren vehicle list finished

--- src/app/mclaren/brands/mclaren-gran-turismo/mclaren-gran-turismo.component.ts
+++ src/app/mclaren/brands/mclaren-gran-turismo/mclaren-gran-turismo.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VehicleService } from "../../../core/services/vehicle.service";
+
+import { Vehicle } from 'src/app/core/models/vehicle.model';
 @Component({
   selector: 'app-mclaren-gran-turismo',
   templateUrl: './mclaren-gran-turismo.component.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MclarenGranTurismoComponent implements OnInit {
 
-  constructor() { }
+  vehicles: Vehicle[] = [];
+  
+
+  constructor(
+    private vehicleService: VehicleService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchGranTurismoVehicles();
+    
+  }
+
+  fetchGranTurismoVehicles(){
+    this.vehicleService.getAllVehicles().subscribe(vehicle => {
+      const array = Object.values(vehicle);
+      for(let i = 0; i<array.length; i++){
+        if(array[i].brand === "McLaren" && array[i].id_vehicle_type === 4){
+          
+          this.vehicles.push(array[i]);
+          
+        }
+      }
+      
+    })
   }
 
 }
--- src/app/mclaren/brands/mclaren-musclecar/mclaren-musclecar.component.ts
+++ src/app/mclaren/brands/mclaren-musclecar/mclaren-musclecar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VehicleService } from "../../../core/services/vehicle.service";
+
+import { Vehicle } from 'src/app/core/models/vehicle.model';
+
 @Component({
   selector: 'app-mclaren-musclecar',
   templateUrl: './mclaren-musclecar.component.html',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MclarenMusclecarComponent implements OnInit {
 
-  constructor() { }
+  vehicles: Vehicle[] = [];
+  
+
+  constructor(
+    private vehicleService: VehicleService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchMuscleCarVehicles();
+    
+  }
+
+  fetchMuscleCarVehicles(){
+    this.vehicleService.getAllVehicles().subscribe(vehicle => {
+      const array = Object.values(vehicle);
+      for(let i = 0; i<array.length; i++){
+        if(array[i].brand === "McLaren" && array[i].id_vehicle_type === 2){
+          
+          this.vehicles.push(array[i]);
+          
+        }
+      }
+      
+    })
   }
 
 }
